02f61980382ffea8c1ca49e7fc1e87909480715c
ramil2319 <ramil2319@gmail.com>
2019-01-29 15:08:01 +0400

    Implemented SQL Repository

---
+++ EcommerceShoppingCart/EcommerceShoppingCart.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,58 @@
+﻿using EcommerceShoppingCart.Core.Contracts;
+using EcommerceShoppingCart.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceShoppingCart.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dBSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            dBSet = context.Set<T>();
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return dBSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dBSet.Attach(t);
+
+            dBSet.Remove(t);
+        }
+
+        public T Find(string Id)
+        {
+            return dBSet.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            dBSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dBSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
--- EcommerceShoppingCart/EcommerceShoppingCart.WebUI/App_Start/UnityConfig.cs
+++ EcommerceShoppingCart/EcommerceShoppingCart.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using EcommerceShoppingCart.Core.Contracts;
 using EcommerceShoppingCart.Core.Models;
 using EcommerceShoppingCart.DataAccess.InMemory;
+using EcommerceShoppingCart.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace EcommerceShoppingCart.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
