7320eb000b268d6541f5fff49447b32507c1f681
ramil2319 <ramil2319@gmail.com>
2019-01-29 17:22:35 +0400

    Added Product Upload

--- EcommerceShoppingCart/EcommerceShoppingCart.WebUI/Controllers/ProductManagerController.cs
+++ EcommerceShoppingCart/EcommerceShoppingCart.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using EcommerceShoppingCart.Core.ViewModels;
 using EcommerceShoppingCart.DataAccess.InMemory;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -38,7 +39,7 @@ namespace EcommerceShoppingCart.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -46,6 +47,12 @@ namespace EcommerceShoppingCart.WebUI.Controllers
             }
             else
             {
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
+
                 context.Insert(product);
                 context.Commit();
 
@@ -72,7 +79,7 @@ namespace EcommerceShoppingCart.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
@@ -85,18 +92,21 @@ namespace EcommerceShoppingCart.WebUI.Controllers
                 {
                     return View(product);
                 }
-                else
+
+                if (file != null)
                 {
-                    productToEdit.Category = product.Category;
-                    productToEdit.Description = product.Description;
-                    productToEdit.Image = product.Image;
-                    productToEdit.Name = product.Name;
-                    productToEdit.Price = product.Price;
-
-                    context.Commit();
-
-                    return RedirectToAction("Index");
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
                 }
+
+                productToEdit.Category = product.Category;
+                productToEdit.Description = product.Description;
+                productToEdit.Name = product.Name;
+                productToEdit.Price = product.Price;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
             }
         }
 
