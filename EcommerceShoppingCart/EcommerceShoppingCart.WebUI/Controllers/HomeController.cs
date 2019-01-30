b09acdf723693ed9eef223019791f17d3f0eaeea
ramil2319 <ramil2319@gmail.com>
2019-01-30 16:36:15 +0400

    Added Product List

--- EcommerceShoppingCart/EcommerceShoppingCart.WebUI/Controllers/HomeController.cs
+++ EcommerceShoppingCart/EcommerceShoppingCart.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EcommerceShoppingCart.Core.Contracts;
+using EcommerceShoppingCart.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,32 @@ namespace EcommerceShoppingCart.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> productContext;
+        IRepository<ProductCategory> productCategoriesContext;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            this.productContext = productContext;
+            productCategoriesContext = productCategoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = productContext.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = productContext.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
