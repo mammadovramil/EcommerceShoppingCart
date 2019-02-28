dcbf581d7c103cea0d66fe2d0a70689af20784e9
ramil2319 <ramil2319@gmail.com>
2019-02-28 12:39:30 +0400

    Created Basket UI

--- EcommerceShoppingCart/EcommerceShoppingCart.WebUI/App_Start/UnityConfig.cs
+++ EcommerceShoppingCart/EcommerceShoppingCart.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using EcommerceShoppingCart.Core.Contracts;
 using EcommerceShoppingCart.Core.Models;
 using EcommerceShoppingCart.DataAccess.InMemory;
 using EcommerceShoppingCart.DataAccess.SQL;
+using EcommerceShoppingCart.Services;
 using System;
 
 using Unity;
@@ -48,6 +49,9 @@ namespace EcommerceShoppingCart.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
